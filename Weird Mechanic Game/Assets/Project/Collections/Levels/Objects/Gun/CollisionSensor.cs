2622c98e8107624125c7ffa32e614c0f391ab64e
N-8-D-e-v <72030291+N-8-D-e-v@users.noreply.github.com>
2021-01-17 13:47:16 -0500

    fixed a couple bugs

--- Weird Mechanic Game/Assets/Project/Collections/Levels/Objects/Gun/CollisionSensor.cs
+++ Weird Mechanic Game/Assets/Project/Collections/Levels/Objects/Gun/CollisionSensor.cs
@@ -6,13 +6,32 @@ public abstract class CollisionSensor : MonoBehaviour
     //Public Fields
     public int Overlaps {private set; get;} = 0;
 
-    private void OnCollisionEnter2D(Collision2D _col) => Overlaps ++;
+    //Tag
+    private const string BLOCKING = "Blocking";
 
-    private void OnCollisionExit2D(Collision2D _col) => Overlaps --;
+    private void OnCollisionEnter2D(Collision2D _col)
+    {
+        if (_col.collider.CompareTag(BLOCKING))
+            Overlaps ++;
+    }
 
-    private void OnTriggerEnter2D(Collider2D _col) => Overlaps ++;
+    private void OnCollisionExit2D(Collision2D _col)
+    {
+        if (_col.collider.CompareTag(BLOCKING))
+            Overlaps --;
+    }
 
-    private void OnTriggerExit2D(Collider2D _col) => Overlaps --;
+    private void OnTriggerEnter2D(Collider2D _col)
+    {
+        if (_col.CompareTag(BLOCKING))
+            Overlaps ++;
+    }
+
+    private void OnTriggerExit2D(Collider2D _col)
+    {
+        if (_col.CompareTag(BLOCKING))
+            Overlaps --;
+    }
 
     protected bool Colliding()
     {
