6175c1e3428ecf00aeda7d998cc9de555e6a54bf
N-8-D-e-v <72030291+N-8-D-e-v@users.noreply.github.com>
2021-01-18 18:42:59 -0500

    Deleted TimeScaleFromMicLevel class

---
+++ Weird Mechanic Game/Assets/Game/General/Time/TimeManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using Game.Inputs.Mic;
+
+namespace Game.General.TimeManagement
+{
+    public class TimeManager : MonoBehaviour
+    {
+        //Singleton
+        private static TimeManager Instance;
+
+        //Assignables
+        private Animator animator;
+
+        //Time Scale
+        [Header("Time Scales")]
+        [SerializeField] private float normalTimeScale = 1f;
+        [SerializeField] private float slowMotionTimeScale = 0.2f;
+        private float targetTimeScale = 1f;
+
+        //Smoothing
+        [Header("Smoothing")]
+        [SerializeField] private float timeScaleSmoothing = 0.2f;
+        private float velocity = 0f;
+
+        //Mic Level
+        [Header("Mic Level")]
+        [SerializeField] private float slowMotionMicThreshold = -65f;
+
+        //Animations
+        private const string SLOW_MOTION = "slow_motion";
+
+        private void Awake()
+        {
+            if (!Instance)
+                Instance = this;
+            else if (Instance != this)
+                Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+
+            animator = GetComponentInChildren<Animator>();
+        }
+        
+        private void Update()
+        {
+            Time.timeScale = Mathf.SmoothDamp(Time.timeScale, targetTimeScale, ref velocity, timeScaleSmoothing);
+            UpdateTimeScale();
+        }
+
+        private void UpdateTimeScale()
+        {
+            bool _slowMotion = MicrophoneData.MicrophoneLevel >= slowMotionMicThreshold;
+            animator.SetBool(SLOW_MOTION, _slowMotion);
+            targetTimeScale = _slowMotion ? slowMotionTimeScale : normalTimeScale;
+        }
+    }
+
+    public enum GameState
+    {
+        Play,
+        Pause
+    }
+}
