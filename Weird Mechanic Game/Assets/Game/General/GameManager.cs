ac1f0787b56a2fafa120e1c87eb9759bd3b764d4
N-8-D-e-v <72030291+N-8-D-e-v@users.noreply.github.com>
2021-01-18 18:09:33 -0500

    started reworking mic input to time scale here

--- Weird Mechanic Game/Assets/Game/General/GameManager.cs
+++ Weird Mechanic Game/Assets/Game/General/GameManager.cs
@@ -10,12 +10,22 @@ namespace Game.General
 
         //Global Fields
         public static GameState GameState = GameState.Play;
-        public static float TimeScale = 1f;
+
+        //Assignables
+        private Animator animator;
 
         //Time Scale
         [Header("Time Scale")]
         [Range(0.01f, 1f)] [SerializeField] private float timeScaleSmoothing = 0.2f;
         private float velocity = 0f;
+
+        //Slow Motion
+        [Header("Slow Motion")]
+        [SerializeField] private float slowMotionThreshold = 0.4f;
+        [SerializeField] private float slowMotionExitThreshold = 0.7f;
+
+        //Animations
+        private const string SLOW_MOTION = "slow_motion";
 
         private void Awake()
         {
@@ -24,6 +34,8 @@ namespace Game.General
             else if (Instance != this)
                 Destroy(gameObject);
             DontDestroyOnLoad(gameObject);
+
+            animator = GetComponentInChildren<Animator>();
         }
         
         private void Update()
@@ -35,7 +47,17 @@ namespace Game.General
         private void UpdateTimeScale()
         {
             float _timeScale = MicrophoneLevelToTimeScale.GetTimeScaleFromMicLevel(MicrophoneData.MicrophoneLevel);
-            // Time.timeScale = Mathf.SmoothDamp(Time.timeScale, _timeScale, ref velocity, timeScaleSmoothing);
+            float _smoothTimeScale = Mathf.SmoothDamp(Time.timeScale, _timeScale, ref velocity, timeScaleSmoothing);
+            Time.timeScale = _smoothTimeScale;
+            UpdatePostProcessing();
+        }
+
+        private void UpdatePostProcessing()
+        {
+            if (Time.timeScale <= slowMotionThreshold)
+                animator.SetBool(SLOW_MOTION, true);
+            else if (Time.timeScale >= slowMotionExitThreshold)
+                animator.SetBool(SLOW_MOTION, false);
         }
     }
 
---
+++ Weird Mechanic Game/Assets/Game/General/PostProcessing/PostProcessing.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Game.General.PostProcessing
+{
+    public class PostProcessing : MonoBehaviour
+    {
+        //Assignables
+        private Volume volume;
+        private Vignette vignette;
+        private ChromaticAberration chromaticAberration;
+        private LensDistortion lensDistortion;
+        private ColorAdjustments colorAdjustments;
+
+        //Values
+        [SerializeField] private float vignetteIntensity = 0.2f;
+        [SerializeField] private float chromaticAberrationIntensity = 0f;
+        [SerializeField] private float lensDistortionIntensity = 0f;
+        [SerializeField] private float colorAdjustmentsContrast = 0f;
+        [SerializeField] private float colorAdjustmentsSaturation = 0f;
+
+        private void Awake()
+        {
+            volume = GetComponent<Volume>();
+            volume.profile.TryGet<Vignette>(out vignette);
+            volume.profile.TryGet<ChromaticAberration>(out chromaticAberration);
+            volume.profile.TryGet<LensDistortion>(out lensDistortion);
+            volume.profile.TryGet<ColorAdjustments>(out colorAdjustments);
+        }
+        
+        private void Update()
+        {
+            vignette.intensity.value = vignetteIntensity;
+            chromaticAberration.intensity.value = chromaticAberrationIntensity;
+            lensDistortion.intensity.value = lensDistortionIntensity;
+            colorAdjustments.contrast.value = colorAdjustmentsContrast;
+            colorAdjustments.saturation.value = colorAdjustmentsSaturation;
+        }
+    }
+}
