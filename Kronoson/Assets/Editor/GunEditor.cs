a06b5ecf1ce2c03dc3ba3c649d9ba70aa10b4448
N8Dev <n8dev.cs@gmail.com>
2021-01-21 15:52:23 -0500

    Finished the shotgun + added a custom inspector

---
+++ Kronoson/Assets/Editor/GunEditor.cs
@@ -0,0 +1,18 @@
+﻿using Game.Levels.Combat.Shooting;
+using UnityEditor;
+
+namespace Game.Editor
+{
+    [UnityEditor.CustomEditor(typeof(Gun))]
+    public class GunEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            Gun _gun = target as Gun;
+            if (_gun.Shots > 1)
+                EditorGUILayout.Slider("Spread", _gun.Spread, 0f, 180f);
+        }
+    }
+}
--- Kronoson/Assets/Game/Levels/Combat/Shooting/Gun.cs
+++ Kronoson/Assets/Game/Levels/Combat/Shooting/Gun.cs
@@ -5,11 +5,12 @@ namespace Game.Levels.Combat.Shooting
     [CreateAssetMenu(fileName = "NewGun", menuName = "Weapons/Gun", order = 0)]
     public class Gun : ScriptableObject
     {
+        public bool IsAutomatic = false;
         public Rigidbody2D Bullet;
         [Range(0, 100)] public float Range = 15f;
         [Range(0, 100)] public float FireRate = 0.2f;
         [Range(0, 100)] public int Shots = 1;
-        [Range(0, 180)] public float Spread = 45f;
-        public bool IsAutomatic = false;
+        [HideInInspector] public float Spread = 45f;
+
     }
 }
