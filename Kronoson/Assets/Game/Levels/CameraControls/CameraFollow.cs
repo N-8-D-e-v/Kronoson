7d69c27ca7043dd9874cd00e670088594fb1eb91
N8Dev <n8dev.cs@gmail.com>
2021-01-24 14:38:51 -0500

    POGO STICK WEAPONS

--- Kronoson/Assets/Game/Levels/CameraControls/CameraFollow.cs
+++ Kronoson/Assets/Game/Levels/CameraControls/CameraFollow.cs
@@ -8,6 +8,9 @@ namespace Game.Levels.CameraControls
     {
         //Assignables
         private Rigidbody2D rb;
+        
+        //Target
+        private Vector3 target;
 
         //Follow Speed
         [Header("Follow Speed")] 
@@ -18,37 +21,25 @@ namespace Game.Levels.CameraControls
         //Bounds
         [Header("Bounds")]
         [SerializeField] private CameraBounds bounds;
-        
-        //Dead Zone
-        [Header("Dead Zone")]
-        [SerializeField] private CameraBounds deadZone;
 
         //Constants
         private const float Z_POS = -10f;
 
         private void Awake() => rb = GetComponent<Rigidbody2D>();
 
-        private void FixedUpdate()
+            private void FixedUpdate()
         {
             Vector3 _target = PlayerData.GetPlayerPosition();
+            Vector3 _pos = rb.position;
             _target.z = Z_POS;
-            
-            if (deadZone.UseBounds)
-            {
-                Vector3 _pos = rb.position;
-                if (_target.x < _pos.x + deadZone.MaxX && _target.x > _pos.x + deadZone.MinX)
-                    _target.x = _pos.x;
-                if (_target.y < _pos.y + deadZone.MaxY && _target.y > _pos.y + deadZone.MinY)
-                    _target.y = _pos.y;
-            }
-            
             if (bounds.UseBounds)
             {
                 _target.x = Mathf.Clamp(_target.x, bounds.MinX, bounds.MaxX);
                 _target.y = Mathf.Clamp(_target.y, bounds.MinY, bounds.MaxY);
             }
+            target = _target;
 
-            rb.MovePosition(Vector3.SmoothDamp(rb.position, _target, ref velocity, followTime));
+            rb.MovePosition(Vector3.SmoothDamp(_pos, target, ref velocity, followTime));
         }
     }
 
