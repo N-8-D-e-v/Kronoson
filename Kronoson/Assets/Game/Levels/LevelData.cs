dba5eb3b34eb28864cd9f70d4c2ed529a900b0eb
N8Dev <n8dev.cs@gmail.com>
2021-01-25 14:14:13 -0500

    level text UI

---
+++ Kronoson/Assets/Game/Levels/LevelData.cs
@@ -0,0 +1,14 @@
+﻿namespace Game.Levels
+{
+    public static class LevelData
+    {
+        //Level
+        private static int level = 1;
+
+        public static void NextLevel() => level += 1;
+
+        public static int GetLevel() => level;
+
+        public static void Reset() => level = 1;
+    }
+}
---
+++ Kronoson/Assets/Game/Levels/UI/LevelText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Game.Levels.UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]   
+    public class LevelText : MonoBehaviour
+    {
+        //Assignables
+        private TextMeshProUGUI levelText;
+
+        private void Awake() => levelText = GetComponent<TextMeshProUGUI>();
+
+        private void Update() => levelText.text = $"LEVEL {LevelData.GetLevel().ToString()}";
+    }
+}
