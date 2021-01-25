4d43333ec976ad95795add1327effe136b8d97b2
N8Dev <n8dev.cs@gmail.com>
2021-01-24 21:15:37 -0500

    finished level 1

---
+++ Kronoson/Assets/Game/Levels/World/Portal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Levels.World
+{
+    public class Portal : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D _col)
+        {
+            //SceneManager.LoadNextScene();
+        }
+    }
+}
