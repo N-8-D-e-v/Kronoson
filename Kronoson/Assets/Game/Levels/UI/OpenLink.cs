e350b3368d177533d7ef9f7e8bf86a8558753fdf
N8Dev <n8dev.cs@gmail.com>
2021-02-05 08:44:22 -0500

    finished 'tutorial'

---
+++ Kronoson/Assets/Game/Levels/UI/OpenLink.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Game.Levels.UI
+{
+    public class OpenLink : MonoBehaviour
+    {
+        //Link
+        [SerializeField] private string link = "https://www.youtube.com/N8Dev";
+
+
+        public void Open() => Application.OpenURL(link);
+    }
+}
